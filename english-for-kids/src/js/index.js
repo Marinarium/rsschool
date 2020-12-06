58e4d8c7fb28a952425461592158f79a2e8edc00
Marinarium <schwmk@gmail.com>
2020-12-06 20:35:57 +0300

    feat: rotate for cards

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -1,15 +1,13 @@
 import '../css/style.css';
 import '../css/style.scss';
-import {
-  showMenu
-} from './showMenu';
-
-import {
-  changeSwitch
-} from './changeSwitch';
+import {showMenu} from './showMenu';
+import {changeSwitch} from './changeSwitch';
+import {rotateCard} from './rotateCard';
 
 showMenu();
 changeSwitch();
+rotateCard();
+
 
 
 
---
+++ english-for-kids/src/js/rotateCard.js
@@ -0,0 +1,33 @@
+module.exports = {
+    rotateCard
+};
+
+function rotateCard() {
+    const mainBlock = document.querySelector('.main');
+    const allCards = document.querySelectorAll('.card');
+    const allRotateArrows = document.querySelectorAll('.card__rotate');
+
+    mainBlock.addEventListener('click', function (event) {
+        let showBackOfCard = function (arrow, card) {
+
+            if(arrow.contains(event.target)){
+                card.classList.toggle('card_rotate')
+                console.log("arrow click");
+            }
+
+            if(card.classList.contains('card_rotate')){
+                card.addEventListener('mouseleave', function _listener() {
+                    card.classList.remove('card_rotate');
+                    card.removeEventListener('mouseleave', _listener);
+                });
+            }
+
+        };
+
+        for (let i = 0; i < allRotateArrows.length; i++) {
+            showBackOfCard(allRotateArrows[i], allCards[i]);
+        }
+    });
+}
+
+
