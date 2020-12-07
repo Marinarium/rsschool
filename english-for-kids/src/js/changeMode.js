a1dadd11b09f039eea43978ac1c2a77332fff435
Marinarium <schwmk@gmail.com>
2020-12-08 01:55:02 +0300

    feat: change view for play mode

---
+++ english-for-kids/src/js/changeMode.js
@@ -0,0 +1,37 @@
+export function changeMode() {
+    const switchControl = document.querySelector('.switch');
+    const checkBox = document.querySelector('.switch__checkbox');
+    const topicsStatus = document.querySelectorAll('.topics__status');
+    const allCards = document.querySelectorAll('.cards');
+    const buttonStart = document.querySelector('.button-start');
+
+    switchControl.addEventListener("click", chooseMode);
+
+    function chooseMode() {
+        console.log("click");
+        setTimeout(function(){
+            if (checkBox.checked) {
+                topicsStatus.forEach(function (item) {
+                    item.classList.add('topics__status_play');
+                });
+
+                allCards.forEach(function (item) {
+                    item.classList.add('card_play');
+                });
+
+                buttonStart.classList.add('button-start_play');
+
+            } else {
+                topicsStatus.forEach(function (item) {
+                    item.classList.remove('topics__status_play');
+                });
+
+                allCards.forEach(function (item) {
+                    item.classList.remove('card_play');
+                });
+
+                buttonStart.classList.remove('button-start_play');
+            }
+        },200);
+    }
+}
--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -5,13 +5,14 @@ import {changeSwitch} from './changeSwitch';
 import {rotateCard} from './rotateCard';
 import {playAudioForWord} from './playAudioForWord';
 import {changeDeckOfCards} from './changeDeckOfCards.js';
+import {changeMode} from './changeMode.js';
 
 showMenu();
 changeSwitch();
 rotateCard();
 playAudioForWord();
 changeDeckOfCards();
-
+changeMode();
 
 
 
